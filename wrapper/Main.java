aef1d035a3409cfcf63f1d9b30e9a485e21a8530
Haris Haseeb <Harishaseeb934@gmail.com>
2026-04-27 19:52:48 +0500

    Learning Wrapper Classes

---
+++ wrapper/Main.java
@@ -0,0 +1,38 @@
+package wrapper;
+
+class Main {
+    public static void main(String args[]){
+//        int a = 10;
+//        Integer aa = a;
+//        System.out.println(aa);
+//
+//        int b = aa;
+//        System.out.println(b);
+
+
+//        String s = "123";
+//
+//        int is = Integer.parseInt(s);
+//        System.out.println(is);
+//
+//        double ds = Double.parseDouble(s);
+//        System.out.println(ds);
+//
+//        String ins = "12A";
+//        try{
+//            int ints = Integer.parseInt(ins);
+//        }catch(NumberFormatException e){
+//            System.out.println("NumberFormatException Error Occurs");
+//        }
+
+
+        Integer a = 100;
+        Integer b = 100;
+        Integer x = 1200;
+        Integer y = 1200;
+
+        System.out.println(a == b);
+        System.out.println(x.equals(b));
+
+    }
+}
